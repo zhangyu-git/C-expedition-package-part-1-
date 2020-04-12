a2628b5e4b84edeeb7d73bd39d20263fbbec101c
zhangyu-git <641346589@qq.com>
2020-04-12 21:24:13 +0800

    6-11

---
+++ 6-11/Teacher.cpp
@@ -0,0 +1,31 @@
+#include "Teacher.h"
+
+Teacher::Teacher(string _name, int _age):
+  m_strName(_name), m_iAge(_age)
+{
+  cout << "Teacher(string _name, int _age)" << endl;
+}
+Teacher::Teacher(const Teacher &tea)
+{
+  cout << "Teacher(const Teacher &tea)" << endl;
+}
+Teacher::~Teacher()
+{
+  cout << "~Teacher()" << endl;
+}
+void Teacher::setName(string _name)
+{
+  m_strName = _name;
+}
+string Teacher::getName()
+{
+  return m_strName;
+}
+void Teacher::setAge(int _age)
+{
+  m_iAge = _age;
+}
+int Teacher::getAge()
+{
+  return m_iAge;
+}
